2d22d8ef94a8d264b9acbf571c5566be5648104d
Eric <40010382+sinbinsam@users.noreply.github.com>
2020-02-24 13:05:19 -0500

    changed timer package

--- jackpot-node/app.js
+++ jackpot-node/app.js
@@ -1,6 +1,7 @@
 const   sql         = require('mssql'),
         express     = require('express'),
         schedule    = require('node-schedule'),
+        CronJob     = require('cron').CronJob,
         axios       = require('axios'),
         path        = require('path'),
         config      = require(path.join(__dirname, './sql.json')),
@@ -25,31 +26,21 @@ CONVERT(money, (PenniesWon / 100.00))AS 'WinInDollars',gamename\
 
 
 
-var rule = new schedule.RecurrenceRule(); //set schedule of query
-    rule.dayOfWeek = [new schedule.Range(0, 6)];
-    rule.minute = [new schedule.Range(0, 59, 3)];
- 
- 
-
-
-    sql.on('error', err => {
-        console.log()
-    })
-
-
-var j = schedule.scheduleJob(rule, async function(){ //execute query
-        const date = new Date()
-    console.log(date + ': ' + '3 minute timer')
+const job = new CronJob('0/3 * * * *', async function() {
+    const d = new Date();
+    console.log(d + '3 minute timer: ')
     const pool = new sql.ConnectionPool(config);
     const poolConnect = pool.connect();
     await poolConnect;
-    sendJackpotQuery(pool, date);
-});
+    sendJackpotQuery(pool);
+})
+
+job.start()
 
 
-async function sendJackpotQuery(pool, date) {
+
+async function sendJackpotQuery(pool) {
     const transaction = new sql.Transaction(pool)
-
         transaction.begin(err => {
             if (err) {
                 console.log(date + ': ' + 'there was an error with transaction:')
@@ -61,9 +52,10 @@ async function sendJackpotQuery(pool, date) {
                         console.log(date + ': ' + 'there was an error querying:')
                         console.log(err)
                     } else {
+                        const date = new Date()
                         if (result.recordset.length > 0) {
                             console.log(date + ': ' + JSON.stringify(result.recordsets));
-                            sendPlayCommand()
+                            jackpotConditional(parseInt(result.recordsets[0].WinInDollars), result.recordsets[0].gamename)
                             pool.close()
                             return
                         } else if (result.recordset.length == 0) {
@@ -85,7 +77,55 @@ async function sendJackpotQuery(pool, date) {
         })
 }
 
+let tracks2500 = [
+    '2500AllYourFriendsV1',
+    '2500FeelAwesomeV1',
+    '2500GenericV1',
+    '2500ItHappenedAgainV1',
+    '2500MomentsAgoV1',
+    '2500WhosNextV1'
+]
 
+let tracks5000 = [
+    '5000AnotherV1',
+    '5000CongratulationsV1',
+    '5000MaybeYouV1',
+    '5000MomentsAgoV1',
+    '5000ThatsHowV1',
+    '5000WayToGoV1',
+    '5000WhosNextV1'
+]
+
+let tracks10000 = [
+    '10000BigCongratsV1',
+    '10000CongratulationsV1',
+    '10000FingerLakesFortuneV1',
+    '10000GenericV1',
+    '10000MomentsAgoV1',
+    '10000TimeToCelebrateV1'
+]
+
+function jackpotConditional(num, gameName) {
+    if (num < 1000) {
+        console.log('jackpot is less than 1000')
+    } else if (num >= 1000) {
+        console.log('jackpot is greater than or equal to 1000')
+    }
+}
+
+function sjackpotConditional(num, gameName) {
+    if (!num || num < 2500) {
+        console.log('no number or less than 1000')
+    } else {
+        if (num >= 2500 && num < 5000) { //if greater than or equal to 2,500 and less than 5,000
+            sendPlayCommand(tracks2500[getRandomInt(tracks2500.length)], num, gameName)
+        } else if (num >= 5000 && num < 10000) { //if greater or equal to 5,000 and less than 10,000
+            sendPlayCommand(tracks5000[getRandomInt(tracks5000.length)], num, gameName)
+        } else if (num >= 10000) { //if greater than or equal to 10,000
+            sendPlayCommand(tracks10000[getRandomInt(tracks10000.length)], num, gameName)
+        }
+    }
+} //tracks[getRandomInt(tracks.length)]
 
 
 function getRandomInt(max) {
@@ -104,11 +144,11 @@ let tracks = [
 ];
 
 
-function sendPlayCommand() {
+function sendPlayCommand(track) {
     let date = new Date()
     axios({
         method: 'get',
-        url: 'http://10.160.27.80:9000/rest/control?cmd=' + tracks[getRandomInt(tracks.length)],
+        url: 'http://10.160.27.80:9000/jackpot/play?fileName=' + track + '&chickenDinner' + '&gameName' + gameName,
       })
       .then((res) => {
           if (res.status === 200) {
---
+++ jackpot-player/RESTV2.brs
@@ -0,0 +1,113 @@
+Function REST_Initialize(msgPort As Object, userVariables As Object, bsp as Object)
+
+    REST = newREST(msgPort, userVariables, bsp)
+    return REST
+
+End Function
+
+
+Function newREST(msgPort As Object, userVariables As Object, bsp as Object)
+
+	' Create the object to return and set it up
+	s 			= 	{}
+	s.version 		= 	0.4
+	s.msgPort 		= 	msgPort
+	s.userVariables 	= 	userVariables
+	s.bsp 			= 	bsp
+	s.ProcessEvent 		= 	REST_ProcessEvent
+
+	's.player = CreateObject("roAudioPlayer")
+	's.player.MapStereoOutput(0)
+
+	s.player = CreateObject("ROAUDIOPLAYERMX")
+	s.player.SetAudioOutput(0)
+	
+	'Zone Msg
+	s.zoneMsgSend 		= 	zoneMsgSend
+
+
+	
+	'HTTP Server
+	s.newREST		=	createobject("roHttpServer", {port : 9000})
+	s.newRest.SetPort(msgPort)
+	s.newREST.AddGetFromEvent({	
+		url_path 	: 	"/jackpot/play"
+		user_data	:	"restControl"	
+	})
+		
+	'Object Name
+	s.objectName 		= 	"REST_object"
+	
+	return s
+
+End Function
+
+REM Event Process Region
+
+Function REST_ProcessEvent(event As Object) as boolean
+
+	retval = false
+	
+	? "Incoming event type is: " type(event)
+
+	if type(event) = "roAssociativeArray" then
+		? event
+		if type(event["EventType"]) = "roString"
+			? "RSCook: " event["EventType"]
+			 if (event["EventType"] = "SEND_PLUGIN_MESSAGE") then
+				if event["PluginName"] = "REST" then
+					pluginMessage$ = event["PluginMessage"]
+					? "SEND_PLUGIN/EVENT_MESSAGE:";pluginMessage$
+					retval = ParsenetCommPluginMsg(pluginMessage$, m)
+				endif
+			endif
+		endif			
+	elseif type(event) = "roHttpEvent" then	
+		'Protect against LFN type settings		
+		if type(event.GetUserData()) = "roString" then			
+			if event.GetUserData() = "restControl" then
+            fileName = event.GetRequestParam("fileName");
+            chickenDinner = event.GetRequestParam("chickenDinner");
+            gameName = event.GetRequestParam("gameName");
+				if fileName <> "" and chickenDinner <> "" and gameName <> "" then
+					m.zoneMsgSend(fileName, chickenDinner, gameName)
+					event.SetResponseBodyString("CMD_Received: "+fileName+", "+chickenDinner+", "+gameName)
+					event.SendResponse(200)
+					retval = true
+				else
+					? "There is no parameter"
+					event.SetResponseBodyString("Invalid parameters")
+					event.SendResponse(200)
+					? "Don't pass this to another plugin"
+					retval = true
+				endif
+			endif		
+		endif			
+	endif
+	return retval	
+	
+End Function
+
+REM Plugin Msg Parser - Not currently used but added here if needed at a later date
+Function ParseRESTPluginMsg(origMsg as string, s as object) as boolean
+	
+	retval = false			
+	return retval
+	
+End Function
+
+REM Send a zone message to Presentation.
+Function zoneMsgSend(fileName$ As String, chickenDinner$ As String, gameName$ As String)
+
+
+
+audio = {
+    filename: "/jackpot_audio/"+fileName$+".wav",
+    QueueNext: 1,
+}
+
+m.player.PlayFile(audio)
+
+
+	'm.player.PlayFile("/music.wav")
+End Function
