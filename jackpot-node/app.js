a01e44e5eb98f2c395e730f306b9d76525f282fd
Eric <40010382+sinbinsam@users.noreply.github.com>
2020-02-24 14:59:44 -0500

    Update app.js

--- jackpot-node/app.js
+++ jackpot-node/app.js
@@ -54,8 +54,8 @@ async function sendJackpotQuery(pool) {
                     } else {
                         const date = new Date()
                         if (result.recordset.length > 0) {
-                            console.log(date + ': ' + JSON.stringify(result.recordsets));
-                            jackpotConditional(parseInt(result.recordsets[0].WinInDollars), result.recordsets[0].gamename)
+                            console.log(date + ': ' + JSON.stringify(result.recordsets[0]));
+                            jackpotConditional(parseInt(result.recordsets[0][0].WinInDollars), result.recordsets[0][0].gamename)
                             pool.close()
                             return
                         } else if (result.recordset.length == 0) {
@@ -105,23 +105,19 @@ let tracks10000 = [
     '10000TimeToCelebrateV1'
 ]
 
-function jackpotConditional(num, gameName) {
-    if (num < 1000) {
-        console.log('jackpot is less than 1000')
-    } else if (num >= 1000) {
-        console.log('jackpot is greater than or equal to 1000')
-    }
-}
 
-function sjackpotConditional(num, gameName) {
+function jackpotConditional(num, gameName) {
     if (!num || num < 2500) {
         console.log('no number or less than 1000')
     } else {
         if (num >= 2500 && num < 5000) { //if greater than or equal to 2,500 and less than 5,000
+            console.log('playing 2500 - 5000')
             sendPlayCommand(tracks2500[getRandomInt(tracks2500.length)], num, gameName)
         } else if (num >= 5000 && num < 10000) { //if greater or equal to 5,000 and less than 10,000
             sendPlayCommand(tracks5000[getRandomInt(tracks5000.length)], num, gameName)
+            console.log('playing 5000 - 10000')
         } else if (num >= 10000) { //if greater than or equal to 10,000
+            console.log('playing 10000')
             sendPlayCommand(tracks10000[getRandomInt(tracks10000.length)], num, gameName)
         }
     }
@@ -144,11 +140,11 @@ let tracks = [
 ];
 
 
-function sendPlayCommand(track) {
+function sendPlayCommand(track, chickenDinner, gameName) {
     let date = new Date()
     axios({
         method: 'get',
-        url: 'http://10.160.27.80:9000/jackpot/play?fileName=' + track + '&chickenDinner' + '&gameName' + gameName,
+        url: 'http://10.160.27.80:9000/jackpot/play?fileName=' + track + '&chickenDinner=' + chickenDinner + '&gameName' + gameName,
       })
       .then((res) => {
           if (res.status === 200) {
